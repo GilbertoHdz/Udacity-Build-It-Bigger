d487b8e7d266b8677c3f7ecb0269c9b5d3ad1d0b
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2020-05-02 17:55:03 -0500

    implment internet check connections state

--- app/src/main/java/com/manitos/dev/gpcendpoint/MainActivity.java
+++ app/src/main/java/com/manitos/dev/gpcendpoint/MainActivity.java
@@ -3,17 +3,28 @@ package com.manitos.dev.gpcendpoint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.manitos.dev.gpcendpoint.api.network.InternetCheck;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar _loader;
+    private TextView _error_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _loader = (ProgressBar) findViewById(R.id.pg_loader);
+        _error_message = (TextView) findViewById(R.id.tv_error_msg);
     }
 
 
@@ -41,6 +52,38 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        checkNetworkConnectionAndGetJokeService();
     }
 
+    private void checkNetworkConnectionAndGetJokeService() {
+        _loader.setVisibility(View.VISIBLE);
+        _error_message.setVisibility(View.GONE);
+
+        new InternetCheck(new InternetCheck.Consumer() {
+            @Override
+            public void accept(Boolean internet) {
+                if (internet) {
+                    // Calling joke api services
+                } else {
+                    showErrorMessage(R.string.error_network_message);
+                }
+            }
+        });
+    }
+
+
+
+
+    private void openJokeDetailScreen() {
+        _loader.setVisibility(View.GONE);
+        _error_message.setVisibility(View.GONE);
+        // TODO IMPLEMENT ACTIVITY JOKE DETAIL
+    }
+
+    private void showErrorMessage(int resMsgId) {
+        String message = getApplicationContext().getString(resMsgId);
+        _error_message.setText(message);
+        _error_message.setVisibility(View.VISIBLE);
+        _loader.setVisibility(View.GONE);
+    }
 }
